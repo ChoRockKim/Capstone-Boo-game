4e6a0d2ee971df16ec03edaaa9c30e95e22d849c
김태헌 <chorock@gimtaeheon-ui-MacBookAir.local>
2026-05-04 18:17:33 +0900

    main screen completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [loaded] = useFonts({
+    NeoDunggeunmo: require("../assets/fonts/NeoDunggeunmo.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) return null;
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,35 @@
 import MainButton from "@/components/Button/MainButton";
-import { ImageBackground, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function Index() {
+  const insets = useSafeAreaInsets();
+  console.log(insets);
   return (
     <ImageBackground
       style={styles.backgroundImage}
       source={require("../assets/images/main-building.png")}
       resizeMode="cover"
     >
-      <SafeAreaView>
-        <MainButton />
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* 메인 타이틀 부분 */}
+        <View style={[styles.titleContainer, { top: insets.top + 19 }]}>
+          <Image
+            style={styles.mainTitle}
+            source={require("../assets/images/main-title.png")}
+            resizeMode="contain"
+          />
+        </View>
+        {/* 버튼 부분 */}
+        <View style={[styles.buttonContainer, { bottom: insets.bottom + 34 }]}>
+          <MainButton color="gray" label="회원가입" width={156} />
+          <MainButton color="blue" label="로그인" width={156} />
+        </View>
       </SafeAreaView>
     </ImageBackground>
   );
@@ -19,5 +38,25 @@ export default function Index() {
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    gap: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mainTitle: {
+    width: 319,
+    height: 136,
+  },
+  titleContainer: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
